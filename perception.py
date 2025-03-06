a28bc0ca723160b6feb3086f44a463625cd71ace
kunzatt <kunzatt2501@gmail.com>
2025-03-06 13:26:42 +0900

    feat: req3

--- perception.py
+++ perception.py
@@ -43,26 +43,17 @@ class IMGParser(Node):
 
         np_arr = np.frombuffer(msg.data, np.uint8)
         img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        '''
-        로직 3. 이미지 색 채널을 gray scale로 컨버팅
-        cv2. 내의 이미지 색 채널 컨터버로 bgr 색상을 gary scale로 바꾸십시오.
+        
+        # 로직 3. 이미지 색 채널을 gray scale로 컨버팅
+        img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
-        img_gray = 
-
-        '''
-
-        '''
-        로직 4. 이미지 resizing
-        cv2를 사용해서 이미지를 원하는 크기로 바꿔보십시오.
-
-        img_resize = 
-        '''
+        # 로직 4. 이미지 resizing
+        img_resize = cv2.resize(img_gray, dsize=(640, 480))
 
         # 로직 5. 이미지 출력 (cv2.imshow)       
-        
         cv2.imshow("img_bgr", img_bgr)
-        # cv2.imshow("img_gray", img_gray)
-        # cv2.imshow("resize and gray", img_resize)       
+        cv2.imshow("img_gray", img_gray)
+        cv2.imshow("resize and gray", img_resize)       
         
         cv2.waitKey(1)
 
