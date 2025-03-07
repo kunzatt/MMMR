0f067c387f746a0fa9a153a101ba812ab62daa01
정영진 <agw1247@naver.com>
2025-03-07 12:42:54 +0900

    Revert "Merge branch 'feature/sub1/정영진-카메라-영상-처리' into 'master'"
    This reverts merge request !10

--- sub1/doyun/catkin_ws/build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == 'c:\\python37':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = 'C:\\Users\\SSAFY\\Desktop\\doyun\\project2\\S12P21A703\\sub1\\doyun\\catkin_ws\\install\\my_package'
