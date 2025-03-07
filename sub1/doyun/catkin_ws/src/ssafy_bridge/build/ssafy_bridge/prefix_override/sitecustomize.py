d48027af4daf11a4fd40579ca880663fdda184a2
tkdtlr1998 <tkdtlr1998@naver.com>
2025-03-07 10:43:12 +0900

    :see_no_evil: [Git] gitignore 추가

---
+++ sub1/doyun/catkin_ws/src/ssafy_bridge/build/ssafy_bridge/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == 'c:\\python37':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = 'C:\\Users\\SSAFY\\Desktop\\doyun\\project2\\S12P21A703\\sub1\\doyun\\catkin_ws\\src\\ssafy_bridge\\install\\ssafy_bridge'
