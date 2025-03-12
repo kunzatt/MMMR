718a0a113a73cef0f2ff2c266e7c46fd1e9985c0
kunzatt <kunzatt2501@gmail.com>
2025-03-12 15:46:52 +0900

    :sparkles: [Feat] SecurityConfig 및 SwaggerConfig 생성

---
+++ BE/src/main/java/com/ssafy/mmmr/global/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.ssafy.mmmr.global.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("MMMR API Documentation")
+				.description("<h3>MMMR Reference for Developers</h3>Swagger를 이용한 MMMR API")
+				.version("v1.0")
+				.contact(new Contact()
+					.name("Support Team")
+					.email("support@mmmr.com")
+					.url("https://lab.ssafy.com/s12-mobility-smarthome-sub1/S12P21A703"))
+				.license(new License()
+					.name("MMMR License")
+					.url("https://lab.ssafy.com/s12-mobility-smarthome-sub1/S12P21A703")))
+			.externalDocs(new ExternalDocumentation()
+				.description("MMMR Documentation")
+				.url("hhttps://lab.ssafy.com/s12-mobility-smarthome-sub1/S12P21A703"))
+			.servers(List.of(
+				new Server().url("/api").description("Local server")
+			));
+	}
+
+}
+
+
