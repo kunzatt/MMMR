3fd413ed48c356f4c4021ff44c0c377d0dc699fe
kunzatt <kunzatt2501@gmail.com>
2025-03-12 16:06:12 +0900

    :card_file_box: [Mod] 오타 수정

--- BE/src/main/java/com/ssafy/mmmr/global/error/code/ErrorCode.java
+++ BE/src/main/java/com/ssafy/mmmr/global/error/code/ErrorCode.java
@@ -17,7 +17,7 @@ public enum ErrorCode {
 	INVALID_TOKEN(401, "유효하지 않은 토큰입니다"),
 	EXPIRED_TOKEN(401, "만료된 토큰입니다"),
 	REFRESH_TOKEN_NOT_FOUND(401, "리프레시 토큰을 찾을 수 없습니다."),
-	EMAIL_EXSIST(400, "이미 존재하는 이메일입니다"),
+	EMAIL_EXIST(400, "이미 존재하는 이메일입니다"),
 	INVALID_PASSWORD(400, "잘못된 비밀번호입니다"),
 	LOGIN_BAD_CREDENTIALS(401, "이메일 또는 비밀번호가 일치하지 않습니다."),
 	LOGIN_FAILED(401, "로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요."),
