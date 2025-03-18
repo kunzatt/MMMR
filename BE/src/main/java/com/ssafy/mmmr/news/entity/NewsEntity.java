222609e0643230140a7a98f2323c6a6b9d23f36f
권희주 <00huiju00@gmail.com>
2025-03-18 11:11:24 +0900

    :bug:[Fix] NewsEntity text, content 컬럼 TEXT 설정

--- BE/src/main/java/com/ssafy/mmmr/news/entity/NewsEntity.java
+++ BE/src/main/java/com/ssafy/mmmr/news/entity/NewsEntity.java
@@ -18,10 +18,10 @@ public class NewsEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @CreatedDate
