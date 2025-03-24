ddf5524f896963a797a1d4d80b674aadd7fbc9a4
권희주 <00huiju00@gmail.com>
2025-03-24 10:17:58 +0900

    :white_check_mark:[Test] @ActiveProfiles("test") 어노테이션 추가

--- BE/src/test/java/com/ssafy/mmmr/MmmrApplicationTests.java
+++ BE/src/test/java/com/ssafy/mmmr/MmmrApplicationTests.java
@@ -2,8 +2,10 @@ package com.ssafy.mmmr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MmmrApplicationTests {
 
 	@Test
