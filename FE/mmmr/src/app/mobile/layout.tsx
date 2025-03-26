1ce478a510bc05e942c670da30e314b9a30e8194
dbwjd08 <dbwjd_0813@naver.com>
2025-03-26 16:58:32 +0900

    :art: [Style] 모바일 폰트 적용

--- FE/mmmr/src/app/mobile/layout.tsx
+++ FE/mmmr/src/app/mobile/layout.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Header from '@/components/mobile/header';
-import Footer from '@/components/mobile/footer';
+import { useState, useEffect } from "react";
+import Header from "@/components/mobile/header";
+import Footer from "@/components/mobile/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const token = localStorage.getItem('token');
+        if (typeof window !== "undefined") {
+            const token = localStorage.getItem("token");
             setIsAuthenticated(!!token); // 토큰이 있으면 true, 없으면 false
         }
     }, []); // 빈 배열을 의존성으로 지정하여 처음 로드될 때 한 번만 실행
 
     return (
         <div className="flex justify-center">
-            <div className="w-full max-w-[500px] h-screen bg-gray-100 flex flex-col">
+            <div className="font-sans w-full max-w-[500px] h-screen bg-gray-100 flex flex-col">
                 <Header />
                 <div className="flex-1 overflow-auto">{children}</div>
                 {isAuthenticated && <Footer />} {/* 로그인 상태일 때 Footer 표시 */}
