014fbadec7583c27bf4355893c771d87b30674df
kunzatt <kunzatt2501@gmail.com>
2025-03-28 15:45:04 +0900

    :construction: [WIP] 프로필 별 스케줄 api 구현중

---
+++ BE/src/main/java/com/ssafy/mmmr/schedules/entity/ScheduleEntity.java
@@ -0,0 +1,11 @@
+package com.ssafy.mmmr.schedules.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "schedules")
+public class ScheduleEntity {
+}
