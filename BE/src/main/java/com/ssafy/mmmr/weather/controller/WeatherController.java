73532c1b592bfcf6816e5bc820ce4126ac6cb9a1
kunzatt <kunzatt2501@gmail.com>
2025-04-01 16:47:53 +0900

    :bug: [Fix] 날씨 API 주소 자동으로 입력되게 수정

--- BE/src/main/java/com/ssafy/mmmr/weather/controller/WeatherController.java
+++ BE/src/main/java/com/ssafy/mmmr/weather/controller/WeatherController.java
@@ -1,12 +1,13 @@
 package com.ssafy.mmmr.weather.controller;
 
+import com.ssafy.mmmr.account.dto.AuthUser;
+import com.ssafy.mmmr.global.annotation.CurrentUser;
 import com.ssafy.mmmr.global.error.dto.ErrorResponse;
 import com.ssafy.mmmr.global.response.ApiResponse;
-import com.ssafy.mmmr.weather.dto.WeatherRequestDto;
 import com.ssafy.mmmr.weather.dto.WeatherResponseDto;
 import com.ssafy.mmmr.weather.service.WeatherService;
+
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,8 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +30,7 @@ public class WeatherController {
 
 	@Operation(
 		summary = "종합 날씨 정보 조회",
-		description = "주소를 기반으로 OpenWeatherMap API를 사용하여 현재 날씨, 기온, 습도, 옷차림 추천, 우산 필요 여부 정보를 제공합니다."
+		description = "사용자 프로필의 주소를 기반으로 OpenWeatherMap API를 사용하여 현재 날씨, 기온, 습도, 옷차림 추천, 우산 필요 여부 정보를 제공합니다."
 	)
 	@ApiResponses({
 		@io.swagger.v3.oas.annotations.responses.ApiResponse(
@@ -87,26 +87,11 @@ public class WeatherController {
 			)
 		)
 	})
-	@PostMapping
-	public ResponseEntity<ApiResponse> getWeather(
-		@Parameter(description = "날씨 정보 요청 데이터", required = true)
-		@io.swagger.v3.oas.annotations.parameters.RequestBody(
-			description = "날씨 정보를 요청할 주소",
-			required = true,
-			content = @Content(
-				mediaType = "application/json",
-				schema = @Schema(implementation = WeatherRequestDto.class),
-				examples = @ExampleObject(
-					value = """
-						{
-							"address": "서울특별시 강남구 테헤란로 212"
-						}
-						"""
-				)
-			)
-		)
-		@RequestBody WeatherRequestDto request) {
-		WeatherResponseDto response = weatherService.getComprehensiveWeather(request.getAddress());
+	@GetMapping
+	public ResponseEntity<ApiResponse> getWeather(@CurrentUser AuthUser authUser) {
+		String address = authUser.getAddress();
+		System.out.println(address);
+		WeatherResponseDto response = weatherService.getComprehensiveWeather(address);
 		return ResponseEntity.ok(new ApiResponse("날씨 정보를 성공적으로 조회했습니다.", response));
 	}
 }
