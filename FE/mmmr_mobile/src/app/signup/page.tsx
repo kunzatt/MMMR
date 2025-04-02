fe3f5bf33d55066c915367065573bacf34c875f9
dbwjd08 <dbwjd_0813@naver.com>
2025-04-02 10:22:38 +0900

    :bug: [Fix] 회원가입 이메일인증 오류 수정

--- FE/mmmr_mobile/src/app/signup/page.tsx
+++ FE/mmmr_mobile/src/app/signup/page.tsx
@@ -72,6 +72,9 @@ export default function SignupPage() {
         try {
             const response = await fetch(API_ROUTES.accounts.codeVerification(email), {
                 method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
                 body: JSON.stringify({ code: emailCode }),
             });
             const data = await response.json();
