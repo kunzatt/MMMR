56bb0722e1ef8b46bd8b0dc21e5435be4619c7b7
dbwjd08 <dbwjd_0813@naver.com>
2025-04-02 17:32:34 +0900

    :bug: [Fix] API 주소 변경

--- FE/mmmr_mobile/src/config/apiRoutes.ts
+++ FE/mmmr_mobile/src/config/apiRoutes.ts
@@ -1,8 +1,8 @@
 // src/config/apiRoutes.ts
 
 // 서버 주소 설정
-//const BASE_URL = "https://j12a703.p.ssafy.io";
-const BASE_URL = "http://70.12.246.80:8088";
+const BASE_URL = "https://j12a703.p.ssafy.io";
+//const BASE_URL = "http://70.12.246.80:8088";
 
 // API 경로 설정
 export const API_ROUTES = {
